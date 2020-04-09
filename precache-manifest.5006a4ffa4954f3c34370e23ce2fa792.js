8122b9d9a06bbfcd28f96c68fd4abf562fbadeba
Ashley <arealhumannotrobot@gmail.com>
2020-04-09 16:19:09 +0800

    Updates

---
+++ precache-manifest.5006a4ffa4954f3c34370e23ce2fa792.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c434023af8987277ee0ae3beb5e06f4",
+    "url": "/RPG/index.html"
+  },
+  {
+    "revision": "fdb080da1d95389b4905",
+    "url": "/RPG/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e94d865b2e168d390bbe",
+    "url": "/RPG/static/js/2.e5910678.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RPG/static/js/2.e5910678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb080da1d95389b4905",
+    "url": "/RPG/static/js/main.56879090.chunk.js"
+  },
+  {
+    "revision": "c05e46d4ab08ab5fd37b",
+    "url": "/RPG/static/js/runtime-main.6a2ca869.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RPG/static/media/logo.5d5d9eef.svg"
+  }
+]);
